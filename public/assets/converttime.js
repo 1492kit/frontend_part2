093853d4848f8ce5ad61eda8edcc63525da7726b
Kirill Jimenez <1492kit@gmail.com>
2024-08-06 15:48:56 +0300

    добавил переменные для последних цифр года, дня, часа, минуты

--- public/assets/converttime.js
+++ public/assets/converttime.js
@@ -1,44 +1,48 @@
 export default function convertTime(postDate, currentDate) {
   const timeDifference = currentDate - postDate;
   const years = timeDifference / 31536000000;
+  const days = timeDifference / 86400000;
+  const hours = timeDifference / 3600000;
+  const minutes = timeDifference / 60000;
+  const yearLastDigit = Math.floor(years).toString().at(-1);
+  const dayLastDigit = Math.floor(days).toString().at(-1);
+  const hourLastDigit = Math.floor(hours).toString().at(-1);
+  const minuteLastDigit = Math.floor(minutes).toString().at(-1);
   if (years >= 1) {
-    if (Math.floor(years).toString().at(-1) === '1') {
+    if (yearLastDigit === '1') {
       return `${Math.floor(years)} год назад`;
     }
-    if (Math.floor(years).toString().at(-1) === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
+    if (yearLastDigit === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
       return `${Math.floor(years)} года назад`;
     }
     return `${Math.floor(years)} лет назад`;
   }
 
-  const days = timeDifference / 86400000;
   if (days >= 1) {
-    if (Math.floor(days).toString().at(-1) === '1') {
+    if (dayLastDigit === '1') {
       return `${Math.floor(days)} день назад`;
     }
-    if (Math.floor(days).toString().at(-1) === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
+    if (dayLastDigit === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
       return `${Math.floor(days)} дня назад`;
     }
     return `${Math.floor(days)} дней назад`;
   }
 
-  const hours = timeDifference / 3600000;
   if (hours >= 1) {
-    if (Math.floor(hours).toString().at(-1) === '1') {
+    if (hourLastDigit === '1') {
       return `${Math.floor(hours)} час назад`;
     }
-    if (Math.floor(hours).toString().at(-1) === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
+    if (hourLastDigit === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
       return `${Math.floor(hours)} часа назад`;
     }
     return `${Math.floor(hours)} часов назад`;
   }
 
-  const minutes = timeDifference / 60000;
   if (minutes >= 1) {
-    if (Math.floor(minutes).toString().at(-1) === '1') {
+    if (minuteLastDigit === '1') {
       return `${Math.floor(minutes)} минуту назад`;
     }
-    if (Math.floor(minutes).toString().at(-1) === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
+    if (minuteLastDigit === '2' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '3' || Math.floor(timeDifference / 31536000000).toString().at(-1) === '4') {
       return `${Math.floor(minutes)} минуты назад`;
     }
     return `${Math.floor(minutes)} минут назад`;
